46d04e137c06283d1b75f485d26fc76edecb171b
ganesh2289 <ganesh2289@gmail.com>
2024-11-03 12:31:01 +0530

    Update stg_customers.sql

--- models/example/stg_customers.sql
+++ models/example/stg_customers.sql
@@ -1,4 +1,8 @@
-SELECT
-  *
-FROM
-  dbt-tutorial.jaffle_shop.customers
+With stg_customers AS (
+   SELECT
+      id,first_name,last_name,
+      CONCAT(first_name, ' ', last_name) as customer_name,
+         FROM dbt-tutorial.jaffle_shop.customers
+)
+
+Select * from stg_customers
