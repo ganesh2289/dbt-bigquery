4941ff2feca0f7d0699e2b95c7ad9284bcc9c286
ganesh2289 <ganesh2289@gmail.com>
2024-11-04 16:00:14 +0530

    Create macro.sql

---
+++ models/example/macro.sql
@@ -0,0 +1,24 @@
+-- Use the `ref` function to select from other models-
+
+SELECT
+    o.ID as O_ID,
+    c.ID,
+    c.Customer_Name,
+    o.user_id,
+    o.Order_Date,
+    o.status,
+    o._etl_loaded_at,
+    p.paymentmethod as payment_method,
+    p.satus as payment_status,
+    p.amount,
+    p.created,
+    p.orderid as order_id,
+    p._batched_at,
+    {{get_date_parts('Order_Date')}} as date_extract
+
+FROM
+    dbt-tutorial.jaffle_shop.orders o
+JOIN
+    {{ ref("stg_customers") }} c ON o.ID = c.ID
+JOIN
+    dbt-tutorial.data_prep.stripe_payments p ON p.id= o.id
