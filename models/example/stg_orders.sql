88dc08b3f98708208dc7eef4404d746dbb0482e9
ganesh2289 <ganesh2289@gmail.com>
2024-11-04 16:12:11 +0530

    Update stg_orders.sql

--- models/example/stg_orders.sql
+++ models/example/stg_orders.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 -- Use the `ref` function to select from other models-
 
 SELECT
@@ -23,27 +22,3 @@ JOIN
 
     JOIN
     dbt-tutorial.stripe.payment p ON p.id= o.id
-=======
--- Use the `ref` function to select from other models-
-
-SELECT
-    o.ID as O_ID,
-    c.ID,
-    c.Customer_Name,
-    o.user_id,
-    o.Order_Date,
-    o.status,
-    o._etl_loaded_at,
-    p.paymentmethod as payment_method,
-    p.satus as payment_status,
-    p.amount,
-    p.created,
-    p.orderid as order_id,
-    p._batched_at
-FROM
-    dbt-tutorial.jaffle_shop.orders o
-JOIN
-    {{ ref("stg_customers") }} c ON o.ID = c.ID
-JOIN
-    dbt-tutorial.data_prep.stripe_payments p ON p.id= o.id
->>>>>>> e67fa7b4e83309531c9dd696094c1f6d715f9bac
